2d3cb7d79fa5c8120ec5988ac174668e80c1c5a4
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-04-04 22:26:03 -0500

    Format

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -54,14 +54,19 @@ export const Navbar = ({ onCartClick }: NavbarProps) => {
               <DropdownMenuContent align="end">
                 <DropdownMenuLabel className="font-normal">
                   <div className="flex flex-col space-y-1">
-                    <p className="text-sm font-medium leading-none">{session.user?.name}</p>
+                    <p className="text-sm font-medium leading-none">
+                      {session.user?.name}
+                    </p>
                     <p className="text-xs leading-none text-muted-foreground">
                       {session.user?.email}
                     </p>
                   </div>
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={() => signOut()} className="text-red-500">
+                <DropdownMenuItem
+                  onClick={() => signOut()}
+                  className="text-red-500"
+                >
                   <LogOut className="mr-2 h-4 w-4" />
                   <span>Log out</span>
                 </DropdownMenuItem>
