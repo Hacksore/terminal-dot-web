c16d81b5e240a3ee1088d4dad7a2f075e411a9e5
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-04-04 22:09:49 -0500

    Remove test script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,13 @@ const client = new Terminal({
   bearerToken: process.env.TERMINAL_BEARER_TOKEN,
 });
 
+const CRON_PRODUCT_ID = "prd_01JD0E7PD4H3XDZA5P5VXSDPQC";
+
 export default async function Home() {
   const { data } = await client.product.list();
 
-  return <App data={data} />;
+  console.log("Fetched products:", data);
+
+  const filteredData = data.filter((product) => product.id !== CRON_PRODUCT_ID);
+  return <App data={filteredData} />;
 }
