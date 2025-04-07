628bc89640853a882970743ed7bd5def2c909e44
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-04-07 04:44:10 -0500

    basic update/create

--- src/app/api/terminal/[...entity]/route.ts
+++ src/app/api/terminal/[...entity]/route.ts
@@ -31,3 +31,35 @@ export async function GET(_, { params }) {
 
   return NextResponse.json({ entity: params.entity });
 }
+
+export async function POST(request, { params }) {
+  const bearerToken = await getAccessToken();
+
+  const body = await request.json().catch(() => null);
+
+  if (!bearerToken) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const client = new Terminal({
+    bearerToken,
+  });
+
+  if (!params?.entity) {
+    return NextResponse.json({ error: "Entity parameter is required" }, { status: 400 });
+  }
+
+  const entityPath = Array.isArray(params.entity) ? params.entity.join("/") : params.entity;
+
+  // if (entityPath === "card/list") {
+  //   const cards = await client.card.list();
+  //   return NextResponse.json({ data: cards.data });
+  // }
+
+  if (entityPath === "address/create") {
+    const card = await client.address.create(body);
+    return NextResponse.json({ data: card });
+  }
+
+  return NextResponse.json({ entity: params.entity });
+}
