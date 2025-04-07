6b27175fcce5f9f1494efb3acd7a17f8cff49a97
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-04-07 05:25:33 -0500

    Moar types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,15 +1,4 @@
 import type { Terminal } from "@terminaldotshop/sdk";
 
-export interface Address {
-  id: string;
-  name: string;
-  street1: string;
-  street2?: string;
-  city: string;
-  state: string;
-  country: string;
-  zip: string;
-  phone?: string;
-}
-
-export type Card = Terminal.Card; 
+export type Address = Terminal.Address;
+export type Card = Terminal.Card;
