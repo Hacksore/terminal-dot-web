ee8922e25e0597321610bbd46aaeabc4d7ea25b6
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-04-16 20:50:07 -0500

    Dont allow checkout if unauthed

--- apps/web/src/app/checkout/page.tsx
+++ apps/web/src/app/checkout/page.tsx
@@ -1,11 +1,19 @@
 import CheckoutPage from "./page.client";
 import { metadata as allMetadata } from "../metadata";
+import { getAuthSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   ...allMetadata,
   title: "Checkout",
 };
 
-export default function Checkout() {
+export default async function Checkout() {
+  const session = await getAuthSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
   return <CheckoutPage />;
 }
