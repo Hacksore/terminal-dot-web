c9264e5efaf22f9e41bf90ce00e37049c5099364
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-04-16 19:15:21 -0500

    Missing env var

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -6,12 +6,14 @@ export const env = createEnv({
     CLIENT_ID: z.string().min(1),
     CLIENT_SECRET: z.string().min(1),
     TERMINAL_BEARER_TOKEN: z.string().min(1),
+    NEXTAUTH_SECRET: z.string().min(1),
   },
   client: {},
   runtimeEnv: {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
     TERMINAL_BEARER_TOKEN: process.env.TERMINAL_BEARER_TOKEN,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
